f2ad07f01c41dc8bb8adf92ed0f7924ba4e8db62
Julian Berman <Julian+git@GrayVines.com>
2011-08-12 01:50:23 -0400

    Fixed for new events syntax.

--- cardboard/tests/util.py
+++ cardboard/tests/util.py
@@ -20,12 +20,13 @@ import logging
 import unittest
 
 import mock
+import panglery
 
 
 class EventHandlerTestCase(unittest.TestCase):
     def setUp(self):
         super(EventHandlerTestCase, self).setUp()
-        self.events = mock.Mock()
+        self.events = mock.Mock(spec=panglery.Pangler)
         self.trigger = self.events.trigger
 
     def failUnexpectedEvents(self, *events):
@@ -33,14 +34,8 @@ class EventHandlerTestCase(unittest.TestCase):
         verb = "was" if len(events) == 1 else "were"
         self.fail("{} {} triggered by {}.".format(events, verb, self.events))
 
-    def assertLastRequestedEventWas(self, e):
-        self.assertLastEventsWere(request(e), event(e))
-
-    def assertLastRequestedEventWasNot(self, e):
-        return self.assertLastEventsWereNot(request(e), event(e))
-
     def assertLastEventsWere(self, *events):
-        events = [[i] for i in events]
+        events = [((), {"event" : event}) for event in events]
         self.assertEqual(events, self.trigger.call_args_list[-len(events):])
 
     def assertLastEventsWereNot(self, *events):
@@ -51,33 +46,12 @@ class EventHandlerTestCase(unittest.TestCase):
         else:
             self.failUnexpectedEvents(*events)
 
-    def assertSubscribed(self, fn, pool=True, **kwargs):
-        if pool:
-            kwargs.update(needs=["pool"])
-
+    def assertSubscribed(self, fn, **kwargs):
         self.assertIn(((fn,), kwargs), self.events.subscribe.call_args_list)
 
 
-class _ANY(object):
-    def __eq__(self, other):
-        return True
-
-    def __repr__(self):
-        return "<any>"
-
-ANY = _ANY()
-
-
 def last_events(game):
-    logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger("event_logger")
-
-    for _, kwargs in game.events.trigger.call_args_list:
-        logger.debug({k : v for k, v in kwargs.iteritems() if k != "pool"})
-
-
-def request(event):
-    return {"request" : event, "pool" : ANY}
-
-def event(event):
-    return {"event" : event, "pool" : ANY}
+    for args, kwargs in game.events.trigger.call_args_list:
+        logging.debug("Called with:\n\targs: {}\n\tkwargs: {}".format(args,
+                                                                      kwargs))
