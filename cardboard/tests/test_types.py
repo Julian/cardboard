a9e206a78acb159757660959e48b66506a6790c4
Julian Berman <Julian+git@GrayVines.com>
2011-09-25 14:27:48 -0400

    Got rid of the stupid Type class.

--- cardboard/tests/test_types.py
+++ cardboard/tests/test_types.py
@@ -2,45 +2,20 @@ import unittest
 
 from cardboard import types as t
 
-class TestType(unittest.TestCase):
-    def test_init(self):
-        y = t.Type(u"Foo")
-        self.assertEqual(y.name, u"Foo")
-
-    def test_repr_str(self):
-        y = t.Type(u"Foo")
-
-        self.assertEqual(repr(y), "<Type: Foo>")
-        self.assertEqual(str(y), "Foo")
-        self.assertEqual(unicode(y), u"Foo")
-
-    def test_hash_eq(self):
-        x = t.Type("Foo")
-        y = t.Type("Foo")
-        z = t.Type("Bar")
-
-        self.assertEqual(x, y)
-        self.assertEqual(hash(x), hash(y))
-
-        self.assertNotEqual(x, z)
-        self.assertNotEqual(hash(x), hash(z))
-
-        self.assertIs(NotImplemented, x.__eq__(object()))
-
+class TestTypes(unittest.TestCase):
     def test_is_permanent(self):
         for type in t.PERMANENTS:
-            self.assertTrue(type.is_permanent)
+            self.assertTrue(t.is_permanent(type))
 
         for type in t.NONPERMANENTS:
-            self.assertFalse(type.is_permanent)
+            self.assertFalse(t.is_permanent(type))
 
     def test_PERMANENTS_NONPERMANENTS(self):
-        self.assertEqual({type.name for type in t.PERMANENTS},
-                         {u"Artifact", u"Creature", u"Enchantment",
-                          u"Land", u"Planeswalker"})
+        self.assertEqual(t.PERMANENTS, {u"Artifact", u"Creature",
+                                        u"Enchantment", u"Land",
+                                        u"Planeswalker"})
 
-        self.assertEqual({type.name for type in t.NONPERMANENTS},
-                         {u"Instant", u"Sorcery"})
+        self.assertEqual(t.NONPERMANENTS, {u"Instant", u"Sorcery"})
 
     def test_TYPES(self):
         self.assertEqual(t.TYPES, t.PERMANENTS | t.NONPERMANENTS)
--- cardboard/types.py
+++ cardboard/types.py
@@ -1,38 +1,16 @@
-class Type(object):
-    def __init__(self, name):
-        super(Type, self).__init__()
-        self.name = str(name)
+ARTIFACT = u"Artifact"
+CREATURE = u"Creature"
+ENCHANTMENT = u"Enchantment"
+LAND = u"Land"
+PLANESWALKER = u"Planeswalker"
 
-    def __eq__(self, other):
-        if not isinstance(other, Type):
-            return NotImplemented
-        return self.name == other.name
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __str__(self):
-        return str(self.name)
-
-    def __unicode__(self):
-        return unicode(self.name)
-
-    def __repr__(self):
-        return "<Type: {.name}>".format(self)
-
-    @property
-    def is_permanent(self):
-        return self in PERMANENTS
-
-ARTIFACT = Type("Artifact")
-CREATURE = Type("Creature")
-ENCHANTMENT = Type("Enchantment")
-LAND = Type("Land")
-PLANESWALKER = Type("Planeswalker")
-
-INSTANT = Type("Instant")
-SORCERY = Type("Sorcery")
+INSTANT = u"Instant"
+SORCERY = u"Sorcery"
 
 PERMANENTS = {ARTIFACT, CREATURE, ENCHANTMENT, LAND, PLANESWALKER}
 NONPERMANENTS = {INSTANT, SORCERY}
 TYPES = PERMANENTS | NONPERMANENTS
+
+
+def is_permanent(type):
+    return type in PERMANENTS
