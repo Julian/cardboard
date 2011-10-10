49fca8259eb4f195d86b93c048154c54c3d97813
Julian Berman <Julian+git@GrayVines.com>
2011-10-10 11:10:08 -0400

    Bumped to a working setup.py.

--- setup.py
+++ setup.py
@@ -1,1 +1,51 @@
-# TODO: requirements: urwid >= 1.0.0, panglery, twisted
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+from cardboard import __version__
+
+
+long_description = (
+"""*cardboard* is [yet another] Magic: The Gathering™ client, which at some
+point will hopefully have things like:
+
+    likely
+    ------
+
+    * multiplayer support
+    * drafting
+    * multiple frontends, including a <canvas> frontend
+
+    slightly less likely
+    --------------------
+
+    * a simple AI
+
+    pie in the sky
+    --------------
+
+    * MTGO-like collectability / economy
+
+"""
+)
+
+setup(
+    name="cardboard",
+    version = __version__,
+    author = "Julian Berman",
+    author_email = "Julian+Cardboard@GrayVines.com",
+    description = "cardboard is a Magic: The Gathering client",
+    license = "MIT/X",
+    url = "http://github.com/Julian/cardboard",
+    long_description = long_description,
+    packages = ["cardboard", "cardboard.cards", "cardboard.db",
+                "cardboard.frontend", "cardboard.tests"],
+    scripts = ["bin/cardboard"],
+    install_requires = [
+        "panglery",
+        "twisted",
+        "urwid >= 1.0.0",
+    ],
+)
