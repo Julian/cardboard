9506bb8204a706b7867d9be3e254624826735fe4
Julian Berman <Julian+git@GrayVines.com>
2011-10-18 12:31:42 -0400

    TextualInfoDisplay and TextualSelector, fixing tests.

--- cardboard/frontend/tests/test_textual.py
+++ cardboard/frontend/tests/test_textual.py
@@ -2,6 +2,7 @@
 
 from StringIO import StringIO
 from textwrap import dedent
+import unittest
 
 import mock
 
@@ -54,8 +55,94 @@ class TestTextualFrontend(GameTestCase):
         formatted_msg = u"    foo\n    bar\n    baz\n\nBla\n"
         self.assertEqual(self.f.getvalue(), formatted_msg)
 
+
+
+class TestTextualInfoDisplay(unittest.TestCase):
+
+    tf = mock.Mock()
+    game = tf.game
+    p1, p2 = game.players = game.turn.order = [mock.Mock(), mock.Mock()]
+    p1.opponents = [p2]
+    tf.player = p1
+    info = t.TextualInfoDisplay(tf)
+
+    def test_card_info(self):
+        class Bar(mock.Mock):
+            name = u"Bar"
+            type = u"Land"
+            subtypes = []
+            supertypes = []
+            colors = ""
+            mana_cost = None
+            abilities = [u"T: Do bar."]
+            power = toughness = loyalty = None
+
+        bar_info = u"Bar\nLand\n\nT: Do bar."
+        self.assertEqual(self.info.card(Bar), bar_info)
+
+        class Foo(mock.Mock):
+            name = u"Foo"
+            type = u"Creature"
+            subtypes = [u"Thing"]
+            supertypes = []
+            colors = "B"
+            mana_cost = u"2BB"
+            abilities = [u"Do foo.", "Do bar."]
+            power = 2
+            toughness = 3
+            loyalty = None
+
+        foo_info = (u"Foo                   2BB\n"
+                    u"Creature — Thing\n\n"
+                    u"Do foo.\n\n"
+                    u"Do bar.\n\n"
+                    u"                      2/3")
+
+        self.assertEqual(self.info.card(Foo), foo_info)
+
+    def test_player_info(self):
+        self.assertEqual(self.info.player(),
+                         u"You: {0.p1}\nOpponent: {0.p2}".format(self))
+
+    def test_player_info_multiple_opponents(self):
+        g = mock.Mock()
+        g.players = g.turn.order = [mock.Mock(), mock.Mock(), mock.Mock()]
+        tf = t.TextualFrontend(g.players[0])
+        tf.game = g
+        info = t.TextualInfoDisplay(tf)
+        g.players[0].opponents = g.players[1:]
+
+        self.assertEqual(
+            info.player(),
+            u"You: {}\nOpponents: {}, {}".format(*g.turn.order)
+        )
+
+    def test_turn_info(self):
+        p = self.game.turn.phase = mock.MagicMock()
+        s = self.game.turn.step = mock.MagicMock()
+
+        p.__str__.return_value, s.__name__ = "Beginning", "untap"
+        p.__len__.return_value = 2
+        self.assertEqual(self.info.turn, u"Phase: Beginning\nStep: Untap")
+
+        p.__str__.return_value = "First Main"
+        p.__len__.return_value = 1
+        self.assertEqual(self.info.turn, u"Phase: First Main")
+
+
+class TestSelector(unittest.TestCase):
+
+    tf = t.TextualFrontend(mock.Mock())
+    p1, p2 = tf.game.players = tf.game.turn.order = [mock.Mock(), mock.Mock()]
+    tf.player = p1
+    s = t.TextualSelector(tf)
+
+    def setUp(self):
+        super(TestSelector, self).setUp()
+        self.f = self.tf.out_file = StringIO()
+
     def test_select(self):
-        self.p1.frontend.in_file = StringIO(u"1\n1, 3\n")
+        self.tf.in_file = StringIO(u"1\n1, 3\n")
 
         class Thing(object):
             def __init__(self, name):
@@ -66,18 +153,18 @@ class TestTextualFrontend(GameTestCase):
 
         c = [Thing(u"bar"), Thing(u"foo"), Thing(u"baz")]
 
-        sel = self.tf.select(c)
+        sel = self.s(c)
         p = u"Select 1 choice.\n\n    1. bar\n    2. foo\n    3. baz\n\n▸▸▸ "
         self.assertEqual(self.f.getvalue(), p.encode("utf-8"))
         self.assertEqual(sel, [c[0]])
 
-        self.assertEqual(self.tf.select(c, how_many=2), [c[0], c[2]])
+        self.assertEqual(self.s(c, how_many=2), [c[0], c[2]])
 
     def test_select_cards(self):
-        self.tf.select = mock.Mock()
-        self.tf.select_cards(range(8), match=lambda i : i % 2 == 0, how_many=2)
+        self.s.choice = mock.Mock()
+        self.s.cards(range(8), match=lambda i : i % 2 == 0, how_many=2)
 
-        args, kwargs = self.tf.select.call_args
+        args, kwargs = self.s.choice.call_args
         kwargs["choices"] = list(kwargs["choices"])
 
         self.assertFalse(args)
@@ -86,10 +173,10 @@ class TestTextualFrontend(GameTestCase):
         )
 
     def test_select_players(self):
-        self.tf.select = mock.Mock()
-        self.tf.select_players(match=lambda p : p == self.p1, how_many=3)
+        self.s.choice = mock.Mock()
+        self.s.players(match=lambda p : p == self.p1, how_many=3)
 
-        args, kwargs = self.tf.select.call_args
+        args, kwargs = self.s.choice.call_args
         kwargs["choices"] = list(kwargs["choices"])
 
         self.assertFalse(args)
@@ -98,71 +185,24 @@ class TestTextualFrontend(GameTestCase):
         )
 
     def test_select_range(self):
-        self.p1.frontend.in_file = StringIO(u"13\n2, 6, 4\n")
+        self.tf.in_file = StringIO(u"13\n2, 6, 4\n1, 2, 3, 4, 5\n")
 
-        sel = self.tf.select_range(1, 21)
+        sel = self.s.range(1, 21)
 
         p = u"Select a number between 1 and 20.\n▸▸▸ "
         self.assertEqual(self.f.getvalue(), p.encode("utf-8"))
         self.assertEqual(sel, [13])
 
-        self.assertEqual(self.tf.select_range(1, 10, how_many=3), [2, 6, 4])
+        self.f = self.tf.out_file = StringIO()
+        sel = self.s.range(1, 10, how_many=3)
 
-    def test_card_info(self):
-        class Bar(mock.Mock):
-            name = u"Bar"
-            type = u"Land"
-            subtypes = []
-            mana_cost = None
-            abilities = [u"T: Do bar."]
-            power = toughness = None
+        p = u"Select 3 numbers between 1 and 9.\n▸▸▸ "
+        self.assertEqual(self.f.getvalue(), p.encode("utf-8"))
+        self.assertEqual(sel, [2, 6, 4])
 
-        # NOTE: don't really care at the moment but maybe later trim spaces
-        bar_info = (u"Bar                 \n"
-                    u"Land\n\n"
-                    u"T: Do bar.")
+        self.f = self.tf.out_file = StringIO()
+        sel = self.s.range(1, 6, how_many=None)
 
-        self.assertEqual(self.tf.card_info(Bar), bar_info)
-
-        class Foo(mock.Mock):
-            name = "Foo"
-            type = "Creature"
-            subtypes = ["Thing"]
-            mana_cost = "2BB"
-            abilities = ["Do foo.", "Do bar."]
-            power = 2
-            toughness = 3
-
-        foo_info = (u"Foo                 2BB\n"
-                    u"Creature — Thing\n\n"
-                    u"Do foo.\n"
-                    u"Do bar.\n"
-                    u"2/3")
-
-        self.assertEqual(self.tf.card_info(Foo), foo_info)
-
-    def test_player_info(self):
-        self.game.start()
-        self.assertEqual(self.tf.player_info(),
-                         u"You: {0.p1}\nOpponent: {0.p2}".format(self))
-
-    def test_player_info_multiple_opponents(self):
-        self.game.add_existing_player(self.p3)
-        self.game.start()
-
-        self.game.turn.order = [self.p1, self.p2, self.p3]
-
-        self.assertEqual(
-            self.tf.player_info(),
-            u"You: {}\nOpponents: {}, {}".format(*self.game.turn.order)
-        )
-
-    def test_turn_info(self):
-        self.game.start()
-
-        self.assertEqual(self.tf.turn_info(), u"Phase: Beginning\nStep: Untap")
-
-        for _ in "untap", "upkeep", "draw":
-            self.game.turn.next()
-
-        self.assertEqual(self.tf.turn_info(), u"Phase: First Main")
+        p = u"Select some numbers between 1 and 5.\n▸▸▸ "
+        self.assertEqual(self.f.getvalue(), p.encode("utf-8"))
+        self.assertEqual(sel, [1, 2, 3, 4, 5])
--- cardboard/frontend/textual.py
+++ cardboard/frontend/textual.py
@@ -1,18 +1,150 @@
 # coding: utf-8
 import sys
-import textwrap
+from textwrap import dedent
 
 import zope.interface
 
+from cardboard.card import characteristics
 from cardboard.frontend import (FrontendMixin, IFrontend, IInfoDisplay,
                                 ISelector)
 from cardboard.util import ANY
 
 
-# FIXME : > 2 Players
+class TextualInfoDisplay(object):
+
+    zope.interface.implements(IInfoDisplay)
+
+    def __init__(self, frontend):
+        super(TextualInfoDisplay, self).__init__()
+
+        self.frontend = frontend
+        self.game = frontend.game
+
+    def card(self, card):
+        info = {k : v or u"" for k, v in characteristics(card).iteritems()}
+
+        if card.mana_cost:
+            info["name"] = info["name"].ljust(20)
+            info["mana_cost"] = info["mana_cost"].rjust(5)
+
+        if card.subtypes:
+            info["subtypes"] = u" — {}".format(", ".join(card.subtypes))
+
+        if card.power or card.toughness:
+            info["pt"] = u"{power}/{toughness}".format(**info).rjust(25)
+        else:
+            info["pt"] = u""
+
+        info["abilities"] = u"\n\n".join(info["abilities"])
+
+        return dedent(u"""
+                       {name}{mana_cost}
+                       {type}{subtypes}
+
+                       {abilities}
+
+                       {pt}
+                       """).format(**info).strip()
+
+    def player(self):
+        self.game.require(started=True)
+
+        s = "s" if len(self.frontend.player.opponents) > 1 else ""
+        opponents = ", ".join(str(o) for o in self.game.turn.order
+                                     if o in self.frontend.player.opponents)
+
+        return dedent("""
+                      You: {}
+                      Opponent{}: {}
+                      """).format(self.frontend.player, s, opponents).strip()
+
+    @property
+    def turn(self):
+        phase = self.game.turn.phase
+        info = ["Phase: {}".format(phase)]
+
+        if len(phase) > 1:
+            step = self.game.turn.step.__name__
+            info.append("Step: {}".format(step.replace("_", " ").title()))
+
+        return "\n".join(info)
+
+    def zone(self):
+        choices = {"Field" : set(self.game.battlefield),
+                   "Exile" : set(self.player.exile),
+                   "Graveyard" : list(self.player.graveyard),
+                   "Hand" : set(self.player.hand)}
+
+        if self._debug:
+            choices["Library"] = list(self.player.library)
+
+        selection = self.menu("Zone Info", None, *sorted(choices))
+
+        selection = self.menu(selection, None, *choices[selection])
+        self.prompt(self.card_info(selection))
+
+
+class TextualSelector(object):
+
+    zope.interface.implements(ISelector)
+
+    def __init__(self, frontend):
+        super(TextualSelector, self).__init__()
+
+        self.frontend = frontend
+        self.game = frontend.game
+
+    def choice(self, choices, how_many=1, duplicates=False):
+        num = u"your" if how_many is None else how_many
+        s = u"s" if how_many is not None and how_many != 1 else ""
+
+        self.frontend.formatted_prompt(
+            header=u"Select {} choice{}.".format(num, s),
+            body=(u"{}. {}".format(i, c) for i, c in enumerate(choices, 1)),
+            end="\n"
+        )
+
+        selections = [int(s) for s in self.frontend.input().split(",")]
+        return [choices[i - 1] for i in selections]
+
+    __call__ = choice
+
+    def cards(self, zone, match=ANY, how_many=1, duplicates=False):
+        return self.choice(
+            choices=(card for card in zone if match(card)),
+            how_many=how_many,
+            duplicates=duplicates,
+        )
+
+    def players(self, match=ANY, how_many=1, duplicates=False):
+        return self.choice(
+            choices=(player for player in self.game.players if match(player)),
+            how_many=how_many,
+            duplicates=duplicates,
+        )
+
+    def range(self, start, stop, how_many=1, duplicates=False):
+        if how_many is None:
+            num = u"some"
+            s = u"s"
+        elif how_many == 1:
+            num = u"a"
+            s = u""
+        else:
+            num = how_many
+            s = u"s"
+
+        prompt = u"Select {} number{} between {} and {}."
+        self.frontend.prompt(prompt.format(num, s, start, stop - 1))
+        return [int(s) for s in self.frontend.input().split(",")]
+
+
 class TextualFrontend(FrontendMixin):
 
-    zope.interface.implements(IFrontend, IInfoDisplay, ISelector)
+    zope.interface.implements(ISelector)
+
+    info = TextualInfoDisplay
+    select = TextualSelector
 
     PS1 = u"▸▸▸ "
     in_file, out_file = sys.stdin, sys.stdout
@@ -39,103 +171,9 @@ class TextualFrontend(FrontendMixin):
             self.prompt(u"\n" + footer)
         self.prompt(end, end="")
 
-    def select(self, choices, how_many=1, duplicates=False):
-        num = u"your" if how_many is None else how_many
-        s = u"s" if how_many is not None and how_many != 1 else ""
-
-        self.formatted_prompt(
-            header=u"Select {} choice{}.".format(num, s),
-            body=(u"{}. {}".format(i, c) for i, c in enumerate(choices, 1)),
-            end="\n"
-        )
-
-        selections = [int(s) for s in self.input().split(",")]
-        return [choices[i - 1] for i in selections]
-
-    def select_cards(self, zone, match=ANY, how_many=1, duplicates=False):
-        return self.select(
-            choices=(card for card in zone if match(card)),
-            how_many=how_many,
-            duplicates=duplicates,
-        )
-
-    def select_players(self, match=ANY, how_many=1, duplicates=False):
-        return self.select(
-            choices=(player for player in self.game.players if match(player)),
-            how_many=how_many,
-            duplicates=duplicates,
-        )
-
-    def select_range(self, start, stop, how_many=1, duplicates=False):
-        self.prompt(
-            u"Select a number between {} and {}.".format(start, stop - 1)
-        )
-        return [int(s) for s in self.input().split(",")]
-
     def started_running(self):
         return self.main_menu()
 
     def main_menu(self):
         selection = self.formatted_prompt("Main Menu", self.player_info(), "Zone Info")
         getattr(self, selection.lower().replace(" ", "_"))()
-
-    def card_info(self, card):
-        mana_cost = card.mana_cost or u""
-        subtypes = pt = u""
-
-        if card.subtypes:
-            subtypes = u" — {}".format(", ".join(card.subtypes))
-
-        if card.power or card.toughness:
-            pt = u"\n{card.power}/{card.toughness}".format(card=card)
-
-        abilities = "\n".join(card.abilities)
-
-        return textwrap.dedent(u"""
-                                {0.name:<20}{mana_cost}
-                                {0.type}{subtypes}
-
-                                {abilities}{pt}
-                                """).strip("\n").format(card,
-                                                        mana_cost=mana_cost,
-                                                        subtypes=subtypes,
-                                                        abilities=abilities,
-                                                        pt=pt)
-
-    def player_info(self):
-        self.game.require(started=True)
-
-        s = "s" if len(self.player.opponents) > 1 else ""
-        opponents = ", ".join(str(o) for o in self.game.turn.order
-                                     if o in self.player.opponents)
-
-        return textwrap.dedent(
-            """
-            You: {}
-            Opponent{}: {}
-            """
-        ).strip("\n").format(self.player, s, opponents)
-
-    def turn_info(self):
-        phase = self.game.turn.phase
-        info = ["Phase: {}".format(phase)]
-
-        if len(phase) > 1:
-            step = self.game.turn.step.__name__
-            info.append("Step: {}".format(step.replace("_", " ").title()))
-
-        return "\n".join(info)
-
-    def zone_info(self):
-        choices = {"Field" : set(self.game.battlefield),
-                   "Exile" : set(self.player.exile),
-                   "Graveyard" : list(self.player.graveyard),
-                   "Hand" : set(self.player.hand)}
-
-        if self._debug:
-            choices["Library"] = list(self.player.library)
-
-        selection = self.menu("Zone Info", None, *sorted(choices))
-
-        selection = self.menu(selection, None, *choices[selection])
-        self.prompt(self.card_info(selection))
