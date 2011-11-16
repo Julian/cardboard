7d600cbd3f17efe81ec4642e4462fdb2939b3f14
Julian Berman <Julian+git@GrayVines.com>
2011-11-15 21:29:58 -0500

    scoped_session and .query

--- cardboard/db/__init__.py
+++ cardboard/db/__init__.py
@@ -1,1 +1,1 @@
-from cardboard.db.core import DB, Base, Session, engine, get_or_create
+from cardboard.db.core import DB, Base, Session, engine
--- cardboard/db/core.py
+++ cardboard/db/core.py
@@ -1,38 +1,24 @@
+import contextlib
 import os.path
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 
 
 DB = os.path.join(os.path.dirname(__file__), "cards.db")
 
+engine = create_engine("sqlite:///{}".format(DB))
+Session = scoped_session(sessionmaker(bind=engine))
+
 
 class Base(object):
+    query = Session.query_property()
+
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
 
 
-def get_or_create(model, session=None, defaults=(), **kwargs):
-    commit = session is None
-
-    if commit:
-        session = Session()
-
-    instance = session.query(model).filter_by(**kwargs).one()
-
-    if instance is None:
-        kwargs.update(defaults)
-        instance = model(**kwargs)
-        session.add(instance)
-
-        if commit:
-            session.commit()
-
-    return instance
-
-
-engine = create_engine('sqlite:///{}'.format(DB))
 Base = declarative_base(bind=engine, cls=Base)
-Session = sessionmaker(bind=engine)
