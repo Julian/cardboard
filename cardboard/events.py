b19726fabfbec3278d1b0f5290f46407d8300338
Julian Berman <Julian+git@GrayVines.com>
2011-12-25 21:19:04 -0500

    Simplifying events.

--- cardboard/events.py
+++ cardboard/events.py
@@ -1,283 +1,175 @@
-from itertools import chain
+"""
+======
+Events
+======
 
+Whenever a game event occurs, the event handler will trigger an event. Game
+objects can subscribe and react to the events (see the :ref:`Implementing a
+Card` document for examples).
 
-__all__ = ["Event", "events"]
+A triggered event may have one or more parameters that describe more specific
+information about the event. The names of these parameters are specific to the
+event.
 
+The different types of events are outlined below, along with an explanation of
+each event's respective parameters.
 
-BuiltinKeyError = KeyError
 
-class KeyError(BuiltinKeyError):
-    """
-    A saner KeyError whose args actually roundtrip.
+Turn Events
+-----------
 
-    """
+Turn events are events that trigger when a game, turn, :term:`phase` or
+:term:`step` begins or ends.
 
-    def __str__(self):
-        return Exception.__str__(self)
+    * game began / game ended
 
+        * event: ``"game began"`` / ``"game ended"``
+        * game: ``<the game object>``
 
-class Event(object):
-    """
-    An event container.
+    * turn began / turn ended
 
-    """
+        * event: ``"turn began"`` / ``"turn ended"``
+        * player: ``<player whose turn is beginning or ending>``
+        * turn: ``<the turn number>``
 
-    def __init__(self, name="", subevents=None, _parent=None, **kwsubevents):
-        super(Event, self).__init__()
+    * phase began / phase ended
 
-        if _parent is not None:
-            _parent = "{.fully_qualified_name}[{!r}]".format(_parent, name)
+        * event: ``"phase began"`` / ``"phase ended"``
+        * phase: ``<the beginning or ending phase>``
+        * player: ``<the active player>``
 
-        self.fully_qualified_name = _parent or name
+    * step began / step ended
 
-        self._subevents = {}
-        self.update(subevents, **kwsubevents)
+        * event: ``"step began"`` / ``"step ended"``
+        * phase: ``<the current phase>``
+        * step: ``<the beginning or ending step>``
+        * player: ``<the active player>``
 
-    def __contains__(self, event):
-        return event in self._subevents.viewvalues()
 
-    def __eq__(self, other):
-        if not isinstance(other, Event):
-            return NotImplemented
-        return self.name == other.name and self._subevents == other._subevents
+Player Events
+-------------
 
-    def __getitem__(self, k):
-        try:
-            return self._subevents[k]
-        except BuiltinKeyError as k:
-            err = "{.fully_qualified_name} has no subevent {}"
-            raise KeyError(err.format(self, k))
+Player events are triggered for in-game events affecting a :term:`player` in
+the game.
 
-    def __setitem__(self, k, v):
-        self._subevents[k] = Event(k, v, _parent=self)
+    * conceded
 
-    def __iter__(self):
-        return iter(self._subevents.viewvalues())
+        The player :term:`conceded <concede>` the game.
 
-    def __len__(self):
-        return len(self._subevents)
+            * event: ``"player conceded"``
+            * player: ``<the conceding player>``
 
-    def __str__(self):
-        return str(self.name)
+    * died
 
-    def __unicode__(self):
-        return unicode(self.name)
+        A player has died.
 
-    def __repr__(self):
-        return "<Event: {.fully_qualified_name}>".format(self)
+            * event: ``"player died"``
+            * player: ``<the dead parrot>``
 
-    @property
-    def name(self):
-        return self.fully_qualified_name.rstrip("']").rpartition("['")[2]
+    * draw
 
-    @property
-    def subevent_names(self):
-        return self._subevents.viewkeys()
+        A player :term:`drew <draw>` a card.
 
-    @property
-    def subevents(self):
-        # evading my .viewvalues __eq__ bug
-        return set(self._subevents.itervalues())
+            * event: ``"draw"``
+            * player: ``<the drawing player>``
 
-    def get(self, k, d=None):
-        return self._subevents.get(k, d)
+    * life gained / life lost
 
-    def update(self, subevents=None, **kwsubevents):
-        if subevents is None:
-            subevents = {}
+        A player gained or lost :term:`life <life, life total>`.
 
-        for k, v in chain(subevents.iteritems(), kwsubevents.iteritems()):
-            self[k] = v
+            * event: ``"life gained"`` / ``"life lost"``
+            * player: ``<the player>``
+            * life: ``<the amount of life (always positive)>``
 
+Player events will also be triggered when a player adds or removes :term:`mana`
+from his :term:`mana pool`.  The mana event will be triggered with:
 
-phase_events = {
+    * event: ``"mana added"`` / ``"mana removed"``
+    * mana: ``"white"`` / ``"blue"`` / ``"black"`` / ``"red"`` / ``"green"`` /
+      ``"colorless"``
+    * player: ``<the player>``
+    * amount: ``<the amount of mana (always positive)>``
 
-    "beginning" : {
-                   "started" : {},
-                   "ended" : {},
 
-                   "untap" : {
-                              "started" : {},
-                              "ended" : {},
-                             },
+Card & Spell Events
+-------------------
 
-                   "upkeep" : {
-                               "started" : {},
-                               "ended" : {},
-                              },
+Card and spell events are triggered for events that are relevant to cards,
+:term:`spells <spell>` and :term`abilities <ability>`.
 
-                   "draw" : {
-                             "started" : {},
-                             "ended" : {},
-                            },
-                  },
-
-    "first_main" : {
-                    "started" : {},
-                    "ended" : {},
-                   },
-
-    "combat" : {
-                "started" : {},
-                "ended" : {},
-
-                "beginning" : {
-                               "started" : {},
-                               "ended" : {},
-                              },
-
-                "declare_attackers" : {
-                                       "started" : {},
-                                       "ended" : {},
-                                      },
-
-                "declare_blockers" : {
-                                      "started" : {},
-                                      "ended" : {},
-                                     },
-
-                "combat_damage" : {
-                                   "started" : {},
-                                   "ended" : {},
-                                  },
-
-                "end" : {
-                         "started" : {},
-                         "ended" : {},
-                        },
-               },
-
-    "second_main" : {
-                     "started" : {},
-                     "ended" : {},
-                    },
-
-
-    "ending" : {
-                   "started" : {},
-                   "ended" : {},
-
-                   "end" : {
-                            "started" : {},
-                            "ended" : {},
-                           },
-
-                   "cleanup" : {
-                                "started" : {},
-                                "ended" : {},
-                               },
-
-               },
-
-}
-
-
-events = Event("all",
-
-               {"card" : {
-                          "cast" : {},
-                          "countered" : {},
-
-                          "status" : {
-                                      "tapped" : {},
-                                      "untapped" : {},
-
-                                      "flipped" : {},
-                                      "unflipped" : {},
-
-                                      "turned_face_up" : {},
-                                      "turned_face_down" : {},
-
-                                      "phased_in" : {},
-                                      "phased_out" : {},
-                                     },
-
-                          "zones" : {
-
-                                     "exile" : {
-                                                "entered" : {},
-                                                "left" : {},
-                                               },
-
-                                     "battlefield" : {
-                                                      "entered" : {},
-                                                      "left" : {},
-                                                     },
-
-                                     "graveyard" : {
-                                                    "entered" : {},
-                                                    "left" : {},
-                                                   },
-
-                                     "hand" : {
-                                               "entered" : {},
-                                               "left" : {},
-                                              },
-
-                                     "library" : {
-                                                  "entered" : {},
-                                                  "left" : {},
-                                                 },
-
-                                     "stack" : {
-                                                "entered" : {},
-                                                "left" : {},
-                                               },
-                                    }
-                         },
-
-                "game" : {
-                          "started" : {},
-                          "ended" : {},
-
-                          "turn" : {
-                                    "started" : {},
-                                    "ended" : {},
-                                    "phase" : phase_events,
-                                   },
-                         },
-
-                "player" : {
-                            "conceded" : {},
-                            "died" : {},
-                            "draw" : {},
-
-                            "life" : {
-                                      "gained" : {},
-                                      "lost" : {},
-                                     },
-
-                            "mana" : {
-                                      "black" : {
-                                                 "added" : {},
-                                                 "removed" : {},
-                                                },
-
-                                      "green" : {
-                                                 "added" : {},
-                                                 "removed" : {},
-                                                },
-
-                                      "red" : {
-                                               "added" : {},
-                                               "removed" : {},
-                                              },
-
-                                      "blue" : {
-                                                "added" : {},
-                                                "removed" : {},
-                                               },
-
-                                      "white" : {
-                                                 "added" : {},
-                                                 "removed" : {},
-                                                },
-
-                                      "colorless" : {
-                                                     "added" : {},
-                                                     "removed" : {},
-                                                    },
-                                     },
-
-                           },
-               })
+    .. note::
+
+        Generally the game does not distinguish (currently) between cards and
+        other game :term:`objects <object>`. They are also referred to as cards
+        for this purpose.
+
+The card and spell events are as follows:
+
+    * cast
+
+        A card was :term:`cast`.
+
+            * event: ``"card cast"``
+            * card: ``<the casted card>``
+            * player: ``<the casting player>``
+
+    * countered
+
+        A spell was :term:`countered <counter>`.
+
+            * event: ``"spell countered"``
+            * spell: ``<the countered spell>``
+
+    * resolved
+
+        A spell :term:`resolved <resolve>`.
+
+            * event: ``"spell resolved"``
+            * spell: ``<the resolving spell>``
+
+
+Additionally, cards have a number of :term:`status` change events that fire
+when a card's status changes. The triggered event will look like:
+
+    * event: ``"status changed"``
+    * card: ``<the card>``
+    * status: one of:
+        * ``"tapped"`` / ``"untapped"``
+        * ``"flipped"`` / ``"unflipped"``
+        * ``"face up"`` / ``"face down"``
+        * ``"phased in"`` / ``"phased out"``
+
+
+Finally, cards will trigger zone change events when their location is changed.
+The card will trigger an event with the zone it is leaving, followed by another
+when it enters its destination:
+
+    * event: ``"entered zone"`` / ``"left zone"``
+    * card: ``<the card>``
+    * zone: ``<the relevant zone>``
+
+
+"""
+
+GAME_BEGAN, GAME_ENDED = "game began", "game ended"
+TURN_BEGAN, TURN_ENDED = "turn began", "turn ended"
+PHASE_BEGAN, PHASE_ENDED = "phase began", "phase ended"
+STEP_BEGAN, STEP_ENDED = "step began", "step ended"
+
+PLAYER_CONCEDED = "player conceded"
+PLAYER_DIED = "player died"
+DRAW = "draw"
+LIFE_GAINED, LIFE_LOST = "life gained", "life lost"
+MANA_ADDED, MANA_REMOVED = "mana added", "mana removed"
+
+CARD_CAST = "card cast"
+SPELL_COUNTERED = "spell countered"
+SPELL_RESOLVED = "spell resolved"
+
+STATUS_CHANGED = "status changed"
+TAPPED, UNTAPPED = "tapped", "untapped"
+FLIPPED, UNFLIPPED = "flipped", "unflipped"
+FACE_UP, FACE_DOWN = "face up", "face down"
+PHASED_IN, PHASED_OUT = "phased in", "phased out"
+
+ENTERED_ZONE, LEFT_ZONE = "entered zone", "left zone"
